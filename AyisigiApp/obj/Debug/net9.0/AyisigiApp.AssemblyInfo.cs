fcd8c037af6224f82cb05f79fef6214dffb18de3
sukruclk7 <celiksukru067@gmail.com>
2025-12-07 16:53:48 +0300

    guncellendi

--- AyisigiApp/obj/Debug/net9.0/AyisigiApp.AssemblyInfo.cs
+++ AyisigiApp/obj/Debug/net9.0/AyisigiApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AyisigiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b098f6ffc7b5d62fa95d835dc269854ae07024fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("AyisigiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AyisigiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
